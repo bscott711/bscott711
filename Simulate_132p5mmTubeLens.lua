fc04515923bb3902b06b2aaff3b84e69c72a0626
bscott711 <40175801+bscott711@users.noreply.github.com>
2023-03-16 15:21:05 -0600

    Tube Lens Simulations

---
+++ Simulate_132p5mmTubeLens.lua
@@ -0,0 +1,427 @@
+# OpticalRayTracer 9.6
+# http://arachnoid.com/OpticalRayTracer
+# 2023.03.16 13:40:25 MDT
+
+program {
+  antialias                 = true
+  askBeforeDeleting         = true
+  beamAngle                 = 0.000000e+00
+  beamCount                 = 4
+  beamWidth                 = 1
+  clipboardGraphicXSize     = 1280
+  colorArrow                = -2147483393
+  colorBaseline             = -16760832
+  colorBeam                 = -4194304
+  colorGrid                 = 1086374080
+  colorHighBackground       = -1
+  colorLensOutline          = 276865279
+  colorLensSelected         = 268484608
+  colorLightSource          = -16776961
+  colorLowBackground        = -16777216
+  colorTerminator           = -16777216
+  decimalPlaces             = 4
+  defaultWindowHeight       = 600
+  defaultWindowWidth        = 900
+  dispScale                 = 0.024747
+  dispersionBeams           = 0
+  divergingSource           = false
+  helpScrollPos             = 0
+  interLensEpsilon          = 1.000000e-06
+  intersectionArrowSize     = 0.050000
+  inverse                   = false
+  maxIntersections          = 64
+  selectedComponent         = 13
+  selectedTab               = 0
+  showControls              = true
+  showGrid                  = true
+  snapValue                 = 0.010000
+  surfEpsilon               = 5.000000e-04
+  tableLineLimit            = 500
+  virtualSpaceSize          = 100.000000
+  windowX                   = 100
+  windowY                   = 100
+  xBeamRotationPlane        = 0.000000e+00
+  xBeamSourceRefPlane       = -60.000000
+  xOffset                   = 32.758228
+  yEndBeamPos               = 1.800000
+  yOffset                   = 0.558780
+  yStartBeamPos             = -1.800000
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 1.227122
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 200mm Tube Lens
+  rightCurvature            = 0
+  rightSphereRadius         = 10.800000
+  rightZValue               = 20.000000
+  symmetrical               = false
+  thickness                 = 0.000000e+00
+  xPos                      = 0.000000e+00
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 0.100000
+  dispersion                = 59.000000
+  function                  = 1
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 60.000000
+  leftZValue                = 20.000000
+  lensRadius                = 20.000000
+  name                      = 132.5mm Focal Plane
+  rightCurvature            = 3
+  rightSphereRadius         = 60.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.100000
+  xPos                      = 13.250000
+  yPos                      = 15.000000
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 0.100000
+  dispersion                = 59.000000
+  function                  = 1
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 60.000000
+  leftZValue                = 20.000000
+  lensRadius                = 50.000000
+  name                      = Initial Focal Plane
+  rightCurvature            = 3
+  rightSphereRadius         = 60.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.100000
+  xPos                      = 20.000000
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = false
+  angle                     = -45.000000
+  centerThickness           = 0.000000e+00
+  dispersion                = 59.000000
+  function                  = 1
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 4.000000
+  name                      = Mirror 2
+  rightCurvature            = 3
+  rightSphereRadius         = 6.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.000000e+00
+  xPos                      = -20.000000
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = true
+  angle                     = -45.000000
+  centerThickness           = 0.000000e+00
+  dispersion                = 59.000000
+  function                  = 1
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 4.000000
+  name                      = Mirror 2
+  rightCurvature            = 3
+  rightSphereRadius         = 6.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.000000e+00
+  xPos                      = -20.000000
+  yPos                      = -15.000000
+}
+
+object {
+  active                    = false
+  angle                     = 45.000000
+  centerThickness           = 0.000000e+00
+  dispersion                = 59.000000
+  function                  = 1
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 4.000000
+  name                      = Mirror 3
+  rightCurvature            = 3
+  rightSphereRadius         = 6.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.000000e+00
+  xPos                      = -30.000000
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = true
+  angle                     = 45.000000
+  centerThickness           = 0.000000e+00
+  dispersion                = 59.000000
+  function                  = 1
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 4.000000
+  name                      = Mirror 3
+  rightCurvature            = 3
+  rightSphereRadius         = 6.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.000000e+00
+  xPos                      = -30.000000
+  yPos                      = 15.000000
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 1.227122
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 10.800000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 200mm Lens
+  rightCurvature            = 0
+  rightSphereRadius         = 10.800000
+  rightZValue               = 20.000000
+  symmetrical               = false
+  thickness                 = 0.000000e+00
+  xPos                      = 5.110000
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 1.227122
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 200mm Tube Lens
+  rightCurvature            = 0
+  rightSphereRadius         = 10.800000
+  rightZValue               = 20.000000
+  symmetrical               = false
+  thickness                 = 0.000000e+00
+  xPos                      = 0.000000e+00
+  yPos                      = 30.000000
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 1.339746
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 0
+  leftSphereRadius          = 10.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 200mm Lens
+  rightCurvature            = 3
+  rightSphereRadius         = 10.800000
+  rightZValue               = 20.000000
+  symmetrical               = false
+  thickness                 = 0.000000e+00
+  xPos                      = 3.510000
+  yPos                      = 30.000000
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 1.227122
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 200mm Tube Lens
+  rightCurvature            = 0
+  rightSphereRadius         = 10.800000
+  rightZValue               = 20.000000
+  symmetrical               = false
+  thickness                 = 0.000000e+00
+  xPos                      = 0.000000e+00
+  yPos                      = 15.000000
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 1.225879
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 0
+  leftSphereRadius          = 20.700000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 200mm Lens
+  rightCurvature            = 0
+  rightSphereRadius         = 20.700000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.000000e+00
+  xPos                      = 4.350000
+  yPos                      = 15.000000
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 1.339746
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 0
+  leftSphereRadius          = 10.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 200mm Lens
+  rightCurvature            = 3
+  rightSphereRadius         = 10.800000
+  rightZValue               = 20.000000
+  symmetrical               = false
+  thickness                 = 0.000000e+00
+  xPos                      = 0.000000e+00
+  yPos                      = -45.000000
+}
+
+object {
+  active                    = true
+  angle                     = -45.000000
+  centerThickness           = 0.000000e+00
+  dispersion                = 59.000000
+  function                  = 1
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 4.000000
+  name                      = Mirror 2
+  rightCurvature            = 3
+  rightSphereRadius         = 6.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.000000e+00
+  xPos                      = -20.000000
+  yPos                      = -30.000000
+}
+
+object {
+  active                    = true
+  angle                     = 45.000000
+  centerThickness           = 0.000000e+00
+  dispersion                = 59.000000
+  function                  = 1
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 4.000000
+  name                      = Mirror 2
+  rightCurvature            = 3
+  rightSphereRadius         = 6.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.000000e+00
+  xPos                      = -30.000000
+  yPos                      = 30.000000
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 1.225879
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 0
+  leftSphereRadius          = 20.700000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 200mm Lens
+  rightCurvature            = 0
+  rightSphereRadius         = 20.700000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.000000e+00
+  xPos                      = 0.000000e+00
+  yPos                      = -30.000000
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 1.227122
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 200mm Tube Lens
+  rightCurvature            = 0
+  rightSphereRadius         = 10.800000
+  rightZValue               = 20.000000
+  symmetrical               = false
+  thickness                 = 0.000000e+00
+  xPos                      = 0.000000e+00
+  yPos                      = -15.000000
+}
+
+object {
+  active                    = true
+  angle                     = -45.000000
+  centerThickness           = 0.000000e+00
+  dispersion                = 59.000000
+  function                  = 1
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 4.000000
+  name                      = Mirror 2
+  rightCurvature            = 3
+  rightSphereRadius         = 6.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.000000e+00
+  xPos                      = -20.000000
+  yPos                      = -45.000000
+}
---
+++ Simulate_169mmTubeLens.lua
@@ -0,0 +1,196 @@
+# OpticalRayTracer 9.6
+# http://arachnoid.com/OpticalRayTracer
+# 2023.03.16 12:26:49 MDT
+
+program {
+  antialias                 = true
+  askBeforeDeleting         = true
+  beamAngle                 = 0.000000e+00
+  beamCount                 = 4
+  beamWidth                 = 1
+  clipboardGraphicXSize     = 1280
+  colorArrow                = -2147483393
+  colorBaseline             = -16760832
+  colorBeam                 = -4194304
+  colorGrid                 = 1086374080
+  colorHighBackground       = -1
+  colorLensOutline          = 276865279
+  colorLensSelected         = 268484608
+  colorLightSource          = -16776961
+  colorLowBackground        = -16777216
+  colorTerminator           = -16777216
+  decimalPlaces             = 4
+  defaultWindowHeight       = 600
+  defaultWindowWidth        = 900
+  dispScale                 = 0.034273
+  dispersionBeams           = 0
+  divergingSource           = false
+  helpScrollPos             = 0
+  interLensEpsilon          = 1.000000e-06
+  intersectionArrowSize     = 0.050000
+  inverse                   = false
+  maxIntersections          = 64
+  selectedComponent         = 6
+  selectedTab               = 0
+  showControls              = true
+  showGrid                  = true
+  snapValue                 = 0.010000
+  surfEpsilon               = 5.000000e-04
+  tableLineLimit            = 500
+  virtualSpaceSize          = 100.000000
+  windowX                   = 100
+  windowY                   = 100
+  xBeamRotationPlane        = 0.000000e+00
+  xBeamSourceRefPlane       = -30.000000
+  xOffset                   = 46.455410
+  yEndBeamPos               = 1.800000
+  yOffset                   = -0.562713
+  yStartBeamPos             = -1.800000
+}
+
+object {
+  active                    = false
+  angle                     = 0.000000e+00
+  centerThickness           = 1.227122
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 200mm Tube Lens
+  rightCurvature            = 0
+  rightSphereRadius         = 10.800000
+  rightZValue               = 20.000000
+  symmetrical               = false
+  thickness                 = 0.000000e+00
+  xPos                      = 0.000000e+00
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 0.100000
+  dispersion                = 59.000000
+  function                  = 2
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 10.000000
+  leftZValue                = 20.000000
+  lensRadius                = 10.000000
+  name                      = Terminal Plane
+  rightCurvature            = 3
+  rightSphereRadius         = 10.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.100000
+  xPos                      = 84.400000
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = false
+  angle                     = 0.000000e+00
+  centerThickness           = 0.100000
+  dispersion                = 59.000000
+  function                  = 2
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = Mirror 1
+  rightCurvature            = 3
+  rightSphereRadius         = 6.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.100000
+  xPos                      = 16.900000
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = false
+  angle                     = 0.000000e+00
+  centerThickness           = 0.100000
+  dispersion                = 59.000000
+  function                  = 2
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = Initial Focal Plane
+  rightCurvature            = 3
+  rightSphereRadius         = 6.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.100000
+  xPos                      = 20.000000
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 0.100000
+  dispersion                = 59.000000
+  function                  = 1
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 6.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 750mm Focal Plane
+  rightCurvature            = 3
+  rightSphereRadius         = 6.000000
+  rightZValue               = 20.000000
+  symmetrical               = true
+  thickness                 = 0.100000
+  xPos                      = 80.000000
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = false
+  angle                     = 0.000000e+00
+  centerThickness           = 0.321841
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 8.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = 750mm Lens
+  rightCurvature            = 0
+  rightSphereRadius         = 39.000000
+  rightZValue               = 20.000000
+  symmetrical               = false
+  thickness                 = 0.000000e+00
+  xPos                      = 3.200000
+  yPos                      = 0.000000e+00
+}
+
+object {
+  active                    = true
+  angle                     = 0.000000e+00
+  centerThickness           = 0.321841
+  dispersion                = 59.000000
+  function                  = 0
+  ior                       = 1.520000
+  leftCurvature             = 3
+  leftSphereRadius          = 8.000000
+  leftZValue                = 20.000000
+  lensRadius                = 5.000000
+  name                      = Lens 2
+  rightCurvature            = 0
+  rightSphereRadius         = 39.000000
+  rightZValue               = 20.000000
+  symmetrical               = false
+  thickness                 = 0.000000e+00
+  xPos                      = 5.000000
+  yPos                      = 0.000000e+00
+}
